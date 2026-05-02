74778dba030da01c877cfa566446992d3e3c4aa1
arshit choubey <arshitchoubey18@gmail.com>
2026-05-02 18:21:07 +0530

    initial project

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const healthRoute = require("./routes/health");
+
+app.use("/health", healthRoute);
+
+app.get("/", (req, res) => {
+  res.json({ message: "CI/CD Pipeline Running 🚀" });
+});
+
+module.exports = app;
